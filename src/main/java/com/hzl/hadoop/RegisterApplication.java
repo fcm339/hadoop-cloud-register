f22642947857ffec3d59777c15b97ec8895dcdc3
12920 <zhongliang.huang@hand-china.com>
2019-12-27 18:40:21 +0800

    fix:初始化

---
+++ src/main/java/com/hzl/hadoop/RegisterApplication.java
@@ -0,0 +1,14 @@
+package com.hzl.hadoop;
+
+/**
+ * description
+ *
+ * @author hzl 2019/12/27 3:44 PM
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RegisterApplication {
+
+
+}
