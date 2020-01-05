d61c7aca6f55084e4d36e79151a742626a29e8c7
12920 <zhongliang.huang@hand-china.com>
2020-01-05 14:42:58 +0800

    fix:添加条形码，二维码测试类

---
+++ src/test/java/ZxingTest.java
@@ -0,0 +1,19 @@
+import com.hzl.hadoop.util.Zxing;
+import org.junit.Test;
+
+/**
+ * description
+ * 条形码，二维码生成和解析测试类
+ * @author hzl 2020/01/05 2:41 PM
+ */
+public class ZxingTest {
+
+	@Test
+	public void zkingtest(String[] args) throws Exception {
+		Zxing.executeForQRFile();
+		// executeForQRBytes();
+
+		//executeForEANFile();
+		Zxing.executeForEANBytes();
+	}
+}
