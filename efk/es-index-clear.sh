7e6d39cd1f6d59fcf20516cf02e16706923d8f35
12920 <zhongliang.huang@hand-china.com>
2020-03-05 16:48:09 +0800

    fix:添加efkl日志监控完成

---
+++ efk/es-index-clear.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+target_dir=`pwd`
+jar_name='register'
+branch_name='local'
+
+
+pid=`ps ax | grep -i $jar_name | grep $target_dir | grep java | grep -v grep | awk '{print $1}'`
+if [ -z "$pid" ] ; then
+        echo "没有服务在运行----------"
+else
+    echo "服务运行中..."
+    kill ${pid}
+fi
+echo "开始git pull..."
+git pull origin master
+mvn clean package -U -DskipTests=true
+cd target
+mv hadoop.jar ${jar_name}.jar
+
+nohup java -jar \
+-Xms512m -Xmx1024m \
+-Dspring.profiles.active=local \
+ ./$jar_name.jar > ../logs/$jar_name.log &
+tail -f ../logs/$jar_name.log
