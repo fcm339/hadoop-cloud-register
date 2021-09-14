61176436666fe4dec2c88fd8d8e42aebdad5e838
12920 <zhongliang.huang@hand-china.com>
2021-09-14 19:40:04 +0800

    完善FactoryBean提示信息
    (cherry picked from commit e09d154)

---
+++ src/main/java/com/hzl/hadoop/util/SqlSessionUtils.java
@@ -0,0 +1,27 @@
+package com.hzl.hadoop.util;
+
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * description
+ *
+ * @author hzl 2021/09/14 11:29 AM
+ */
+@Component
+public class SqlSessionUtils {
+
+	@Autowired
+	@Qualifier("clusterSqlSessionFactory")
+	SqlSessionFactory sqlSessionFactory;
+
+	public  SqlSession getSqlSessionBatch(){
+
+		SqlSession sqlSession=sqlSessionFactory.openSession(ExecutorType.BATCH, true);
+		return sqlSession;
+	}
+}
